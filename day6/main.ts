2cf4d16831c7947a3f294076467988dbb38ee54c
chrisyoungbrighte <chris.young@brighte.com.au>
2023-12-07 10:57:39 +1100

    make day 6 more efficient

--- day6/main.ts
+++ day6/main.ts
@@ -10,7 +10,14 @@ function parseInput(input:string): Race[] {
     const distances = distanceString.split(' ').map(str => parseInt(str.trim())).filter(s => !isNaN(s));
     return times.map((time, i) => ({ time, distance: distances[i] }));
 }
-const waysToWin = (race:Race) => [...Array(race.time).keys()].filter(time => (race.time -time) * time > race.distance).length;
+const waysToWin = (race:Race) => {
+    let waysToWin = 0;
+    for (let i = 0; i < race.time; i++) {
+        const distance = i * (race.time - i);
+        if (distance > race.distance) waysToWin++;
+    }
+    return waysToWin;
+}
 export function part1(input: string): number {
     return parseInput(input).reduce((total, race) => {
         return total * waysToWin(race);
@@ -22,6 +29,5 @@ export function part2(input: string): number {
         time: parseInt(races.reduce((total, race) => `${total}${race.time}`, '')),
         distance: parseInt(races.reduce((total, race) => `${total}${race.distance}`, '')),
     }
-    console.log(race);
     return waysToWin(race);
 }
