232cd005f7d37d1356a6866429e490ceaa98879c
chrisyoungbrighte <chris.young@brighte.com.au>
2023-12-13 10:03:05 +1100

    day 8 part 2

--- day8/main.test.ts
+++ day8/main.test.ts
@@ -2,19 +2,29 @@ import { describe, expect, test } from "bun:test";
 import { part1, part2 } from "./main";
 
 describe("day", () => {
+  describe("part 1", () => {
     const input = `LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
-  describe("part 1", () => {
     test('returns 6', () => {
         expect(part1(input)).toEqual(6);
     })
   });
   describe("part 2", () => {
-    test('returns 0', () => {
-        expect(part2(input)).toEqual(0);
+    const input = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+    test('returns 6', () => {
+        expect(part2(input)).toEqual(6);
     })
   });
 });
--- day8/main.ts
+++ day8/main.ts
@@ -1,35 +1,51 @@
 type node = {
-    name: string,
     left: string,
     right: string,
 }
+type Nodes = Record<string, node>;
 
-function parseInput(input: string): [string[], node[]] {
+function parseInput(input: string): [string[], Nodes] {
     const [directions, nodes] = input.split('\n\n');
-    return [directions.split(''), nodes.split('\n').map(node => {
+    return [directions.split(''), nodes.split('\n').reduce((acc, node) => {
         const [name, children = ''] = node.split(' = ');
         const [left, right] = children.split(', ');
-        return { name, left: left.replace('(', ''), right: right.replace(')','') }
-    })];
+        acc[name] = { left: left.replace('(', ''), right: right.replace(')','') } as node;
+        return acc;
+    }, {} as Nodes)];
 }
 
-
-export function part1(input: string): number {
-    const [directions, nodes] = parseInput(input);
-    let step = 0, nodeName = 'AAA';
-    while (nodeName !== 'ZZZ') {
+const getSteps = (nodeName: string, directions: string[], nodes: Nodes, end: (node: string) => boolean): number => {
+    let step = 0;
+    while (!end(nodeName)) {
         for (const direction of directions) {
-            if (direction === 'L') {
-                nodeName = nodes.find(node => node.name === nodeName)?.left ?? nodeName;
-            } else if (direction === 'R') {
-                nodeName = nodes.find(node => node.name === nodeName)?.right ?? nodeName;
-            }
+            nodeName = getNextNode(nodeName, direction, nodes);
             step++;
         }
     }
     return step;
 }
 
+export function part1(input: string): number {
+    const [directions, nodes] = parseInput(input);
+    return getSteps('AAA', directions, nodes, (node) => node === 'ZZZ');
+}
+
+const getNextNode = (nodeName: string, direction: string, nodes: Nodes): string => {
+    return direction === 'L' ? nodes[nodeName]?.left ?? nodeName : nodes[nodeName]?.right ?? nodeName;
+};
+
 export function part2(input: string): number {
-    return 0;
+    const [directions, nodes] = parseInput(input);
+    let nodeNames = Object.keys(nodes).filter(n => n.match(`A$`));
+    console.log(nodeNames);
+    const steps = nodeNames.map(nodeName => getSteps(nodeName, directions, nodes, (node) => node[2] === 'Z'));
+    return lowestCommonMultiple(steps);
+}
+
+function lowestCommonMultiple(arr: number[]): number {
+  return arr.reduce((acc, n) => (acc * n) / gcd(acc, n));
+}
+
+function gcd(a: number, b: number): number {
+  return b === 0 ? a : gcd(b, a % b);
 }
